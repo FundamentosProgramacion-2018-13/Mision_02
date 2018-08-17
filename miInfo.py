dbef72b0f565ab1d82063c174ed5894b7754abb4
JesusZabdiel <A01374964@itesm.mx>
2018-08-17 08:24:57 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -2,9 +2,9 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
-print ("Mi nombre es: Jesus Zabdiel Sanchez Chavez" , "Con matrícula: A01374964")
-print ("de la carrera Ingenieria en Sistemas Computacionales, ISC"
-print ("Preparatoria de procedencia: PrepaTec Bicultural")
+print ("Mi nombre es: Jesus Zabdiel Sanchez Chavez." , "Con matrícula: A01374964")
+print ("De la carrera Ingenieria en Sistemas Computacionales, ISC.")
+print ("Preparatoria de procedencia: PrepaTec Bicultural.")
 print ("""Me gusta mucho jugar futbol y escuchar diferentes tipos de musica
 Soy una persona muy activa, me gustas estar aprendiendo todo el tiempo y
-tambien, me gusta mucho leer articulos tecnologicos"")
+tambien, me gusta mucho leer articulos tecnologicos.""")
