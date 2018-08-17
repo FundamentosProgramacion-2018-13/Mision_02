bc482e7f9cc3ab59479007b64e05438f2bc401ef
JesusZabdiel <A01374964@itesm.mx>
2018-08-17 15:28:59 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jesus Zabdiel Sanchez Chavez, A01374964
+# Descripcion: Programa que que muestra mi informacion.
 
 # Escribe tu programa después de esta línea.
+
+Va = int (input("Teclea la velocidad del auto en km/h: "))
+d7 = Va * 7
+d45 = Va * 4.5
+T = 791/Va
+
+print ("La distancia que recorreas en 7 hrs sera: ", d7)
+print ("La distancia que recorreras en 4.5 hrs sera: ", d45)
+print ("El tiempo que tardaras en recorrer 791 km sera: ", T)
