463950bf6ff47a8d77b413d03ae0f3bde3a17a6a
A01747290 <42151769+DpalmeITESM@users.noreply.github.com>
2018-08-16 19:25:08 -0500

    Update coordenadas.py

--- coordenadas.py
+++ coordenadas.py
@@ -6,6 +6,7 @@ E:x1,y1,x2,y2
 S:distancia
 E/S:
 distancia=raiz ((x2-x1)**2+(y2-y1)**2)
+
 Algoritmo:
 Obtener variables x1, x2, y1 y y2 del usuario y asigarlos
 Restar los 1 de los 2
@@ -14,10 +15,10 @@ Sumarlos y elevarlos a 0.5
 Reportar resultado
 """
 
-X1 = int(input("X1:"))
-Y1 = int(input("Y1:"))
-X2 = int(input("X2:"))
-Y2 = int(input("Y2:"))
+X1 = float(input("X1:"))
+Y1 = float(input("Y1:"))
+X2 = float(input("X2:"))
+Y2 = float(input("Y2:"))
 total = ((X2-X1)**2)+((Y2-Y1)**2)
 total = total ** 0.5
-print("Distancia entre los puntos: " + str(total))
+print("Distancia entre los puntos: " + "{:,.4f}%".format(total))
