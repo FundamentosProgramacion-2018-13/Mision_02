164144ecc679e67e4d18daed46ced1518b1dade9
A01376718-Silvia <42352922+A01376718-Silvia@users.noreply.github.com>
2018-08-18 16:49:19 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Silvia Ferman Muñoz, A01376718
+# Descripcion: Calcular las distancias y tiempo de un auto utilizando la formula de la velocidad (v=d/t)
 
 # Escribe tu programa después de esta línea.
+
+v = int(input("Cual es la velocidad del auto en km/hr:")
+d1 = v * 7
+d2 = v * 4.5
+t = 791/v
+
+print("La distancia recorrida en 7hrs es: %.2f" % d1)
+print("La distancia recorrida en 4.5hrs es: %.2f" % d2)
+print("El tiempo que se tarda el auto en recorrer 791km es: %.2f" % t)
