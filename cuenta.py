307dcca077d2a3c5c23e65cc6bcab564dc29cec7
A01377744 <42352935+A01377744@users.noreply.github.com>
2018-08-17 23:13:08 -0500

    Cuenta 03

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: alejandroToricesOliva, A01377744
+# Descripcion: cuenta de un restaurante con propina, IVA, total
 
 # Escribe tu programa después de esta línea.
+costo = float(input("Introduzca el costo de la comida: "))
+
+propina = costo * .13
+IVA = costo * .15
+total = costo + propina + IVA
+
+print("Costo de su comida: $%3.2f" % costo)
+print("Propina: $%3.2f" % propina)
+print("IVA: $%3.2f" % IVA)
+print("Total a pagar: $%3.2f" % total)
