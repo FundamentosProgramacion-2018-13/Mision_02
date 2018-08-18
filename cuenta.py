5131879b9666f6607cbf75bcd127867748bafd26
FA0AE <42308682+FA0AE@users.noreply.github.com>
2018-08-18 15:59:18 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -4,10 +4,10 @@
 # Escribe tu programa después de esta línea.
 
 totalComida = int(input ("¿Cuánto fue en total? "))
-print ("Su subtotal es: ", str(totalComida))
+print ("Su subtotal es: $", float(totalComida))
 propina = totalComida * .13
-print ("El monto de propia es ", str(propina))
+print ("El monto de propina es $", float(propina))
 iva = totalComida * .15
-print ("El monto de iva es: ", str(iva))
+print ("El monto de iva es: $", float(iva))
 precioPagar = totalComida + propina + iva
-print ("Su cuenta total es: ", str(precioPagar))
+print ("Su cuenta total es: $", float(precioPagar))
