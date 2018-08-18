43f9066d7109e0656770bf8d783051c386a08bc7
JesusZabdiel <A01374964@itesm.mx>
2018-08-17 19:32:29 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jesus Zabdiel Sanchez Chavez, A01374964
+# Descripcion: Programa que calcula el porcentaje de mujeres y homnbres inscritos y el total.
 
 # Escribe tu programa después de esta línea.
+
+mujeres = int (input ("Cuantas mujeres estan inscritas: "))
+hombres = int (input ("Cuantos hombres estan inscritos "))
+
+total = hombres + mujeres
+PorMujeres = (100 * mujeres) / total
+PorHombres = (100 * hombres) / total
+
+print ("El numero total de alumnos inscritos es:" , total)
+print ("Porcentaje de mujeres: %.2f" % (PorMujeres))
+print ("Porcentaje de hombres: %.2f" % (PorHombres))
