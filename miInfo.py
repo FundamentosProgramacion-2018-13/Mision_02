0603a9adf37de00c7dd4322601e9025756083d2f
A01376718-Silvia <42352922+A01376718-Silvia@users.noreply.github.com>
2018-08-18 16:22:10 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Silvia Ferman Muñoz, A01376718
+# Descripcion: Un algoritmo que pregunte informacion sobre mi. 
 
 # Escribe tu programa después de esta línea.
+
+print("Nombre: Silvia Elena Ferman Muñoz")
+print("Matricula: A01376718")
+print("Carrera: ITI")
+print("Escuela de Procedencia: Instituto Thomas Jefferson")
+print("""Descripcion
+Me gustan mucho las peliculas de accion, mi favorita es "Rapidos y Furiosos"
+Toda mi vida e hecho algun tipo de deporte.""")
