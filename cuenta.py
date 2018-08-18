ae34817299810616c23295087c8d880886397483
FA0AE <42308682+FA0AE@users.noreply.github.com>
2018-08-18 15:52:24 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -2,3 +2,12 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+
+totalComida = int(input ("¿Cuánto fue en total? "))
+print ("Su subtotal es: ", str(totalComida))
+propina = totalComida * .13
+print ("El monto de propia es ", str(propina))
+iva = totalComida * .15
+print ("El monto de iva es: ", str(iva))
+precioPagar = totalComida + propina + iva
+print ("Su cuenta total es: ", str(precioPagar))
