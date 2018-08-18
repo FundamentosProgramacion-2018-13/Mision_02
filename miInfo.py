9c35e94ca9a18b9d471f095f94cc6421fcdc3397
ZoeCD <42352932+ZoeCD@users.noreply.github.com>
2018-08-18 15:41:37 -0500

    Archivo resuelto
    Zoe Caballero
    A01747247
    grupo 04

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Zoe Caballero Dominguez, A1747247
+# Descripcion: Este programa imprime en la pantalla ciertos datos personales, tales como nombre, matricula, preparatoria y una pequeña descripcion
 
 # Escribe tu programa después de esta línea.
+print ("""Nombre:
+Zoe Caballero Dominguez""")
+print ("""Matricula:
+A01747247""")
+print ("""Carrera:
+ISC""")
+print ("""Escuela de procedencia:
+Instituto Tepeyac""")
+print ("""Descripcion:
+Me gusta mucho leer y dibujar. Me gusta la tecnologia y estoy interesada en el desarrollo de software y en ciberseguridad.""")
