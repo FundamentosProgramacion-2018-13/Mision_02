99eeb8ae693a2563250a16e000334f392682e7a8
A01376718-Silvia <42352922+A01376718-Silvia@users.noreply.github.com>
2018-08-18 17:11:39 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Silvia Ferman Muñoz, A01376718
+# Descripcion: Calcular el costo TOTAL de una comida
 
 # Escribe tu programa después de esta línea.
+
+tcomida = int(input("Cual es el total de la comida:"))
+p = tcomida * 0.13
+iva = tcomida * 0.15
+T = tcomida + p + iva
+
+print("El subtotal es:", tcomida)
+print("La propina es: %.2f" % p)
+print("El IVA es: %.2f" % iva)
+print("El Total a pagar es: %.2f" % T)
