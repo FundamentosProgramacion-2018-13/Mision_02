6cab0869d7d155471b89843d1145eebf0127ad17
IrmaGC <42380949+IrmaGC@users.noreply.github.com>
2018-08-19 14:40:29 -0500

    Problema 1
    Irma Gomez Carmona 
    A01747743
    Grupo: 02

--- miInfo.py
+++ miInfo.py
@@ -2,3 +2,16 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+
+print ("""Nombre:
+ Irma Gomez Carmona""")
+print ("""Matrícula:
+ A01747743""")
+print ("""Carrera:
+ ITI""")
+print ("""Escuela de Procedencia:
+ Plantel Azteca""")
+print ("""Descripción:
+ Me gusta ver películas
+ Entrené TaeKwonDo por 8 años, también me gusta la natación
+ Me interesan mucho las Tecnologías de la Información, y la Ecología""")
