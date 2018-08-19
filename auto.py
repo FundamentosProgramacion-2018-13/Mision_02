07e501cf2cba2ec9b85234fbdbcddd8f85c01359
IrmaGC <42380949+IrmaGC@users.noreply.github.com>
2018-08-19 15:25:35 -0500

    Ejercicio 2

--- auto.py
+++ auto.py
@@ -2,3 +2,13 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+
+velocidad=float(input("Velocidad del vehiculo en km/hr: "))
+
+distancia1=velocidad*7
+distancia2=velocidad*4.5
+tiempo= 791/velocidad
+
+print ("Distancia que recorrera en 7 horas: %.1f" %(distancia1))
+print ("Distancia que recorrera en 4.5 horas: %.1f" % (distancia2))
+print ("Tiempo que tardará en recorrer 791 km: %.1f" %(tiempo))
