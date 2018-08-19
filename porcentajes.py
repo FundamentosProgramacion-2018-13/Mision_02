ffe3ed3dd17034125321726cc48542930d03bc66
A01377942 <42352923+A01377942@users.noreply.github.com>
2018-08-19 14:07:44 -0500

    Update porcentajes.py
    Luis Jonathan Rosas Ramos
    A01377942
    LuJu 11:30 Grupo 04

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: luisJonathanRosasRamos, a01377942
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+
+NMujeres=float(input("Cuantas Mujeres estan Inscritas: "))
+NHombres=float(input("Cuantos hombres estan inscritos: "))
+
+Total =NMujeres+NHombres
+
+PMujeres = NMujeres*100/Total
+PHombres = NHombres*100/Total
+
+print("El número de alumnos inscritos es de: ", Total)
+print("El procentaje de mujeres es de: %5.1f "%(PMujeres), "%")
+print("El porcentaje de hombres es de: %5.1f "%(PHombres), "%")
