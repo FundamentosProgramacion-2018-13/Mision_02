325d845ff4219d236ad7d76a7b363dda860dd574
A01377942 <42352923+A01377942@users.noreply.github.com>
2018-08-19 14:09:51 -0500

    Create coordenadas.py

---
+++ coordenadas.py
@@ -0,0 +1,14 @@
+# Autor: luisJonathanRosasRamos, a01377042
+# Descripcion: Conseguir la distancia entre dos coordenadas en base a su formula
+
+# Escribe tu programa después de esta línea.
+
+
+x1 = int(input("Ingresa x1: "))
+y1 = int(input("Ingresa y1: "))
+x2 = int(input("Ingresa x2: "))
+y2 = int(input("Ingresa y2: "))
+
+d = ((x1-x2)**2+(y2-y1)**2)**0.5
+
+print("la distancia es de: %5.4f"%d)
