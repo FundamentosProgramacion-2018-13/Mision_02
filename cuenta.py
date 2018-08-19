8972934865e0303e70e0e2160173a0f83d902bd6
SebastianMaciasI <42380996+SebastianMaciasI@users.noreply.github.com>
2018-08-19 15:50:20 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Sebastian Macias Ibarra, A01376492
+# Descripcion: Obtener la propina, el IVA y el total a pagar.
 
 # Escribe tu programa después de esta línea.
+costoComida= int(input("Costo de la comida: $"))
+
+propina = costoComida * 0.13
+IVA = costoComida * 0.15
+total = costoComida + IVA + propina
+
+print("Costo de su comida: $", costoComida)
+print("Propina: $", propina)
+print("IVA: $", IVA)
+print("Total a pagar: $", total)
