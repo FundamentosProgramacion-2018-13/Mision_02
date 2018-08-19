2b32c38bcf61eb8a41509ba8ccb40354015754af
MarcoGlez <42380940+MarcoGlez@users.noreply.github.com>
2018-08-19 18:55:51 -0500

    Update auto.py
    Marco González Elizalde
    A01376527
    Grupo 02

--- auto.py
+++ auto.py
@@ -2,3 +2,13 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+Vel = input("Velocidad del auto en km/h: ")
+velocidad = float(Vel)
+
+distancia1 = velocidad * 7
+distancia2 = velocidad * 4.5
+tiempo = 791 / velocidad
+
+print("Distancia recorrida en 7 hrs: ", distancia1, " km")
+print("Distancia recorrida en 4.5 hrs: ", distancia2, " km")
+print("Tiempo para recorrer 791 km: ", tiempo , " hrs")
