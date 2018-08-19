bc36db1d0bd31aad60086c84b865737fcc525ca4
A01377942 <42352923+A01377942@users.noreply.github.com>
2018-08-19 13:26:04 -0500

    Update auto.py
    Luis Jonathan Rosas Ramos
    A01377942
    LuJu 11:30 Grupo 04

--- auto.py
+++ auto.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: luisJonathanRosasRamos, A01377042
+# Descripcion: Obtener la distacia con una velocidad de 115 en el tiempo de 7, 4.5 y el tiempo necesario para recorrer una cantidad de kilometros
 
 # Escribe tu programa después de esta línea.
+
+v = float(input("Cual fue la velocidad: "))
+
+
+print("Velocidad del auto en Km/h:", v)
+d = v*7
+print("Distancia recorrida en 7 hrs: ", d, "Km")
+di = v+4.5
+print("Distancia recorrida en 4.5 hrs: " ,di, "Km")
+t = 791/v
+print("Tiempo para recorrer 791 km: %5.4f" %(t),"hrs")
