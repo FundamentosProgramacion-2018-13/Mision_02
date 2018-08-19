a190d391b6832494b4760f6732b289dea2bd62ee
SebastianMaciasI <42380996+SebastianMaciasI@users.noreply.github.com>
2018-08-19 15:42:13 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Sebastian Macias Ibarra, A01376492
+# Descripcion: Sacar los kilómetros recorridos para 7 y 4.5 horas y el tiempo para 791 km
 
 # Escribe tu programa después de esta línea.
+velocidad = int (input ("Escribe la velocidad del auto en km/h: "))
+
+distancia1 = velocidad * 7
+distancia2 = velocidad * 4.5
+tiempo = velocidad / 791
+
+print(" ")
+print("Velocidad del auto en km/h: ", velocidad)
+print("Distancia recorrida por el auto en 7 h: ", distancia1, "km")
+print("Distancia recorrida por el auto en 4.5 h: ", distancia2, "km")
+print("Tiempo necesario para recorrer 791 km: ", tiempo, "horas")
