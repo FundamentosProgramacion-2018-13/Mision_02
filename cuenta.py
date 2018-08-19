dd558babc33ceb5513e9258a1a509ab41e93cb8a
A01377942 <42352923+A01377942@users.noreply.github.com>
2018-08-19 13:40:25 -0500

    Update cuenta.py
    Luis Jonathan Rosas Ramos 
    A01377942
    LuJu 11:30 Grupo 04

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: luisJonathanRosasRamos, A01377942
+# Descripcion: Con el costo total relizar una nueva operacion, sumandole el IVA y propina
 
 # Escribe tu programa después de esta línea.
+
+TotalComida = float(input("Cuanto fue el total de la comida: "))
+
+propina = TotalComida*0.13
+
+IVA = TotalComida*0.15
+
+print("El subtotal: ", TotalComida)
+print("La propina es de: ", propina)
+print("El Iva es de: ", IVA)
+print("El total a pagar es de: ", TotalComida+propina+IVA)
