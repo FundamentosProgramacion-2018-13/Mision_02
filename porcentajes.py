5aa41e6d7140d2f6823a48460b2b590000782c73
IrmaGC <42380949+IrmaGC@users.noreply.github.com>
2018-08-19 16:13:03 -0500

    Ejercicio 4

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Irma Gómez Carmona, A01747743
+# Descripcion: Se necesita calcular el porcentaje de hombres y mujeres inscritos, teniendo como referencia el número de hombres y mujeres.
 
 # Escribe tu programa después de esta línea.
+
+mujer= int (input("No. de mujeres inscritas: "))
+hombre= int (input("No. de hombres inscritos: "))
+totalInscritos =mujer+hombre
+promedioH = hombre*100/totalInscritos
+promedioM = mujer*100/totalInscritos
+
+print ("Total de inscritos: %d" % (totalInscritos))
+print ("Porcentaje de mujeres inscritas: %.1f" %(promedioM),"%")
+print ("Porcentaje de hombres inscritos: %.1f" %(promedioH),"%")
