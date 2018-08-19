8f069e433fdc54d63c4823fe1b00fda0aa8174ad
IrmaGC <42380949+IrmaGC@users.noreply.github.com>
2018-08-19 15:49:14 -0500

    Ejercicio 3
    Irma Gomez Carmona 
    A01747743
    Grupo: 02

--- cuenta.py
+++ cuenta.py
@@ -2,3 +2,12 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+
+costoi=float(input("Costo de su comida: "))
+propina=costoi*0.13
+iva=costoi*0.15
+total=costoi+propina+iva
+
+print ("Propina: $%.2f"%(propina))
+print ("IVA: $%.2f"%(iva))
+print ("Total a pagar: $%.2f"%(total))
