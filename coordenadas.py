a58858fc31629ebb2b8d99498beefd0181628222
SebastianMaciasI <42380996+SebastianMaciasI@users.noreply.github.com>
2018-08-19 17:25:06 -0500

    Coordenadas
    Sebastian Macias
    A01376492
    Grupo 02

--- coordenadas.py
+++ coordenadas.py
@@ -1,4 +1,14 @@
-#Sebastian Macias Ibarra, A01376492
-#
+# Autor: Sebastian Macias Ibarra, A01376492
+# Descripcion: Calcular la distancia entre coordenadas
 
-#Escribe tu 
+# Escribe tu programa después de esta línea.
+x1 = int(input("Escribe x1: "))
+x2 = int(input("Escribe x2: "))
+y1 = int(input("Escribe y1: "))
+y2 = int(input("Escribe y2: "))
+
+x = (x2 - x1)**2
+y = (y2 - y1)**2
+distancia = (x + y)**0.5
+
+print ("Distancia: %.4f" % distancia)
