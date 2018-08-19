d1e8e4afec1190d65f6070d6f5f37499042f6fe0
SebastianMaciasI <42380996+SebastianMaciasI@users.noreply.github.com>
2018-08-19 14:54:38 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,19 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Sebastian Macias Ibarra, A01376492
+# Descripcion: El problema consiste imprimir mi información
 
 # Escribe tu programa después de esta línea.
+print(" ")
+print("   ***Infomración del alumno***")
+print(" ")
+print("- Nombre del alumno: ")
+print("   Sebastian Macias Ibarra")
+print("- Matrícula: ")
+print("   A01376492")
+print("- Preparatoria de procedencia: ")
+print("   Prepa Tec, programa bicultural")
+print("- Carrera")
+print("   ISC")
+print("- Descripción del alumno: ")
+print('''   Me gusta la parte lógica que se lleva a cabo al momento de programar, 
+también me gustan ver los avances tecnológicos y su aplicación en la vida.
+Mis hobbies son: Tocar el piano, baliar, dibujar, nadar, leer (ciencia ficción} y escuchar música (variada)''')
