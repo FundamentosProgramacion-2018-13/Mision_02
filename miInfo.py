9f0babf03b93c37c4eabc528f763cecaf3a80f22
A01377942 <42352923+A01377942@users.noreply.github.com>
2018-08-19 12:56:33 -0500

    Update miInfo.py
    Luis Jonathan Rosas Ramos
    A01377942
    LuJu 11:30 Grupo 04

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: luisJonathanRosasRamos, A01377942
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+print("Nombre:")
+print('Luis Jonathan Rosas Ramos')
+print('Matricula:')
+print('A01377942')
+print('Carrera: ')
+print("ISC")
+print('Escuela de procedencia: ')
+print('Prepa Tec, programa Bicultural')
+print('Descripcion: ')
+print('Me gusta el deporte, algunos como escalada, natacion y ciclismo.')
+print('Soy apasionado por la tecnologia y los avances de tipo númerico.')
