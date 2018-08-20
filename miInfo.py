398f48cd6658340cc8d8656a7c7c171c00335f1f
SrCervantes <42380924+SrCervantes@users.noreply.github.com>
2018-08-19 19:40:15 -0500

    Update miInfo.py
    Luis Ricardo Chagala Cervantes
    A01376951
    Grupo: 02

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,15 @@
 # Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Descripcion:
+#1.	Imprime Nombre: Luis Ricardo Chagala Cervantes
+#2.	Imprime Matricula: A01376951.
+#3.	Imprime Carrera: ISDR
+#4.	Imprime Escuela de procedencia: Prepa Tec, programa Bicultural
+#5.	Imprime Descripción: Me gustan todas las ciencias y amo la tecnología. Me gusta bailar, escuchar música, practicar todos los deportes y leer libros con tema gótico.
+
 
 # Escribe tu programa después de esta línea.
+print ("Nombre: Luis Ricardo Chagala Cervantes.")
+print ("Matricula: A01376951.")
+print ("Carrera: ISDR.")
+print ("Escuela de procedencia: Prepa Tec, programa bicultural.")
+print ("Descripción: Me gustan todas las ciencias y amo la tecnología. Me gusta bailar, escuchar musica, practicar todos los deportes y leer libros con tema gotico.")
