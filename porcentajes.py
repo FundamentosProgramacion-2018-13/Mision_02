2ca4fcc3b71b891cb991f59190446270f70ecd39
A01748760 <42352919+A01748760@users.noreply.github.com>
2018-08-20 18:02:39 -0500

    Problema 4
    David Rodriguez Fragoso - A01748760

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: David Rodriguez Fragoso, A01748760
+# Descripcion: Problema 4
 
 # Escribe tu programa después de esta línea.
+
+hombres = int(input("Número de hombres: "))
+mujeres = int(input("Número de mujeres: "))
+total = hombres+mujeres
+porcentajeHombres = (hombres/total)*100
+porcentajeMujeres = (mujeres/total)*100
+
+print("Hombres inscritos: ", hombres)
+print("Mujeres inscritas: ", mujeres)
+print("Total inscritos: ", total)
+print("Porcentaje de hombres: ", porcentajeHombres, "%")
+print("Porcentaje de mujeres: ", porcentajeMujeres, "%")
