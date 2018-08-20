15bcaf4d1c44215400e96da065813a2881efb66d
A01748760 <42352919+A01748760@users.noreply.github.com>
2018-08-20 17:25:17 -0500

    Problema 1
    David Rodriguez Fragoso - A01748760

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: David Rodriguez Fragso, A01748760
+# Descripcion: Problema 1
 
 # Escribe tu programa después de esta línea.
+
+print ("Nombre: "
+       "David Rodriguez Fragoso")
+print ("Matrícula: "
+       "A01748760")
+print ("Carrera: "
+       "ISDR")
+print ("Escuela de procedencia: "
+       "Colegio Alemán Alexander von Humboldt")
+print ("Descripción:"
+       "Me gustan los juegos de video, salir con amigos y tocar guitarra. No me agradan los lugares con mucho sol y "
+       "prefiero salir de noche. Aparte de los videojuegos me apasionan los coches y manejar.")
