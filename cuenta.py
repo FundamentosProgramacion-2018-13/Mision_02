49c56f464779ef33b98a4653d6c1eb92b8f436dd
MarcoGlez <42380940+MarcoGlez@users.noreply.github.com>
2018-08-19 19:17:28 -0500

    cuenta.py Terminado

--- cuenta.py
+++ cuenta.py
@@ -2,3 +2,13 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+subtotal = float(input("Costo de la comida: "))
+
+tip = subtotal * 0.13
+IVA = subtotal * 0.15
+total = subtotal + tip +IVA
+
+print("""Propina: $%.02f
+Cantidad de IVA: $%.02f
+Total de la cuenta: $%.02f"""
+      %(tip, IVA, total))
