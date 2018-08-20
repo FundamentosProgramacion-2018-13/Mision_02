e36e226dd0c1b2186581efb2a9663fb32a55a39e
MarcoGlez <42380940+MarcoGlez@users.noreply.github.com>
2018-08-19 19:32:14 -0500

    porcentajes.py Terminado

--- porcentajes.py
+++ porcentajes.py
@@ -2,3 +2,14 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+mujeresI = int(input("No. de mujeres inscritas: "))
+hombresI = int(input("No. de hombres inscritos: "))
+
+total = mujeresI + hombresI
+mujeresP = float(mujeresI / total * 100)
+hombresP = float(hombresI / total * 100)
+
+print("""Total de personas inscritas: %d
+Porcentaje de mujeres: %.01f porciento 
+Porcentaje de hombres: %.01f porciento """
+      %(total,mujeresP,hombresP))
