75af6a4ae1ee952695a094c40c6355a17df20e89
A01748760 <42352919+A01748760@users.noreply.github.com>
2018-08-20 17:23:16 -0500

    Problema 2
    David Rodriguez Fragoso - A01748760

--- auto.py
+++ auto.py
@@ -1,4 +1,18 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: David Rodriguez Fragoso, A01748760
+# Descripcion: Problema 2
 
 # Escribe tu programa después de esta línea.
+
+#t = tiempo
+#v = velocidad
+#d = distancia
+
+v = int(input("¿Cuál es la velocidad del auto en km/h?: "))
+d1 = v*7
+d2 = v*4.5
+t = 781/v
+
+print ("Velocidad del auto en km/h: ", v)
+print ("Distancia recorrida en 7hrs: ", d1, "km")
+print ("Distancia recorrida en 4.5hrs: ", d2, "km")
+print ("Tiempo para recorrer 791km: ", t, "hrs")
