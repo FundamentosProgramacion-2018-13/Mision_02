14fa376e032c2a5b13ec7c720fff384279066f43
A01748760 <42352919+A01748760@users.noreply.github.com>
2018-08-20 17:36:55 -0500

    Problema 3
    David Rodriguez Fragoso - A01748760

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: David Rodriguez Fragoso, A01748760
+# Descripcion: Problema 3
 # Escribe tu programa después de esta línea.
+
+#Propina = 13%
+#IVA = 15%
+
+comida = int(input("Costo de su comida: "))
+propina = comida*0.13
+iva = comida*0.15
+total = comida+propina+iva
+
+print ("Costo de su comida: $", comida)
+print ("Propina: $", propina)
+print ("IVA: $", iva)
+print ("Total a pagar: $", total)
