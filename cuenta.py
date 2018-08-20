d138c59e155f6e8c0043f47c1fb7d664d38173e7
SrCervantes <42380924+SrCervantes@users.noreply.github.com>
2018-08-19 19:37:35 -0500

    Update cuenta.py
    Luis Ricardo Chagala Cervantes
    A01376951
    Grupo: 02

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,22 @@
 # Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Descripcion:
+#1.	Leer el costo de la comida.
+#2.	Calcula la propina multiplicando el costo por 13 y dividir el resultado entre 100.
+#3.	Calcula el IVA multiplicando el costo por 15 y dividir el resultado entre 100.
+#4.	Suma el costo de la comida más la propina más el IVA para calcular el total a pagar.
+#5.	Imprime Propina.
+#6.	Imprime IVA.
+#7.	Imprime Total a pagar.
+
 
 # Escribe tu programa después de esta línea.
+Comida = int(input("Costo de su comida: "))
+
+propina = (13 * Comida) / 100
+IVA = (15 * Comida) / 100
+
+total = Comida + propina + IVA
+
+print ("Propina = " , propina)
+print ("IVA = " , IVA)
+print ("Total a pagar = " , total)
