747185ccc957499505b543a115267b73de453ee7
Michelle Sánchez <42380961+A01376622@users.noreply.github.com>
2018-08-19 23:42:51 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Michelle Sánchez Guerrero, A01376622
+# Descripcion: Programa que calcula el costo total de una comida, incluyendo la propina y el IVA.
 
 # Escribe tu programa después de esta línea.
+
+comida = int(input("Costo de la comida: "))
+
+propina = comida * 0.13
+iva = comida * 0.15
+total = comida + propina + iva
+
+print("Subtotal: $%.02f" % (comida))
+print("Propina: $%.02f" % (propina))
+print("IVA: $%.02f" % (iva))
+print("Total a pagar: $%.02f" % (total))
