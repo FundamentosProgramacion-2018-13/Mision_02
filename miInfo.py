d3cfb71eac0aee49b644e97b7e9d1b8819f03ae1
jlmata <42380985+jlmata@users.noreply.github.com>
2018-08-21 17:24:03 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jose Luis Mata Lomeli, A01377205
+# Descripcion: Imprimir los datos dados por el usuario.
 
 # Escribe tu programa después de esta línea.
+
+Nombre= input("Nombre: ")
+Matricula= input("Matricula: ")
+Carrera= input("Carrera de estudio: ")
+Antigua_School= input("Escuela de procedendia: ")
+Yo_soy= input("Descripcion personal (2 o 3 lineas): ")
+
+print (Nombre)
+print (Matricula)
+print (Carrera)
+print (Antigua_School)
+print (Yo_soy)
