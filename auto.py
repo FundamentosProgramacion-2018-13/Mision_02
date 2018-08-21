92484e5ea4a0fae66dd30ce7c6ec0056fb7031ac
jlmata <42380985+jlmata@users.noreply.github.com>
2018-08-21 17:15:56 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,21 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: Jose Luis Mata Lomelí, A01377205
+# Descripcion: Sacar la distancia y el tiempo de una velocidad definida por el usuario
 # Escribe tu programa después de esta línea.
+
+velocidad= int(input("Escriba la velocidad del vehiculo: "))
+
+tiempo_1= int("7")
+tiempo_2= float("4.5")
+distancia= float("791")
+
+distancia_formula_a= velocidad*tiempo_1
+distancia_formula_b= velocidad*tiempo_2
+
+tiempo_formula= distancia/velocidad
+
+
+print("Velocidad del vehiculo: ", (velocidad))
+print("Distancia recorrida en", (tiempo_formula)," horas es:", (distancia_formula_a))
+print("Distancia recorrida en", (tiempo_2), " horas es:" , (distancia_formula_b))
+print("Tiempo para recorrer ", (distancia) ,"seria: ", (tiempo_formula))
+
