e5f1af4cd818eda107fba02362f73f8d8fe66f57
Mariana Paola Caballero Cabrera A01376544 <42322583+marianac99@users.noreply.github.com>
2018-08-20 21:22:01 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Mariana Caballero Cabrera, A01376544
+# Descripcion: A continuación haré un programa que pregunte al usuario el precio de su comida y le calcule cuanto va a pagar de IVA y propina para sacar su total
 
 # Escribe tu programa después de esta línea.
+
+subTotal = input float("Precio de la comida: ")
+
+iva = subTotal*.15
+propina = subTotal*.13
+total = subTotal + iva + propina
+
+print ("IVA:  %5.2f" % (iva))
+print ("Propina:  %5.2f"  %(propina))
+print (" Su total es de $:   %5.2f" %(total))
