86893d277d055911d3eeaa58bb48716016b3f94c
A01748760 <42352919+A01748760@users.noreply.github.com>
2018-08-20 19:13:34 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -12,7 +12,7 @@ d1 = v*7
 d2 = v*4.5
 t = 781/v
 
-print ("Velocidad del auto en km/h: ", v)
-print ("Distancia recorrida en 7hrs: ", d1, "km")
-print ("Distancia recorrida en 4.5hrs: ", d2, "km")
-print ("Tiempo para recorrer 791km: ", t, "hrs")
+print ("Velocidad del auto en km/h: %4.1f"% (v))
+print ("Distancia recorrida en 7hrs: %4.1f"% (d1), "km")
+print ("Distancia recorrida en 4.5hrs: %4.1f"% (d2), "km")
+print ("Tiempo para recorrer 791km: %4.1f"% (t), "hrs")
