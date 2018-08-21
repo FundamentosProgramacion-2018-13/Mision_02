5dbce937bccca09cd97a282a0a65d088c20b20df
Carlos <42352946+CBGO2@users.noreply.github.com>
2018-08-20 22:52:05 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -2,3 +2,14 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+
+print (""""Nombre:
+       Carlos Badillo García""")
+print ("""Matrícula:
+       A01377618""")
+print ("""Carrera:
+       Ingenieria en Sistemas Computacionales""")
+print ("""Escuela de porcedencia:
+       Prepa Tec, programa bicultural""")
+print (""""Descripción:
+       Soy un chavo amante de los videojuegos, la musica (todo tipo), los libros de fantasia y de los deportes... """)
