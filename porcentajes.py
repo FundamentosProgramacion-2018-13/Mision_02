4c8254a904ffd7732b05ce7d0b4cd4f46082ff1a
A01376466 <42380929+A01376466@users.noreply.github.com>
2018-08-20 19:47:29 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ivan Honc Ayón, A01376466
+# Descripcion: Este programa calcula porcentajes de mujeres y hombres en un grupo.
 
 # Escribe tu programa después de esta línea.
+
+numerom = float(input("Dame el número de mujeres inscritas: "))
+numeroh = float(input("Dame el número de hombres inscritos: "))
+
+total = numerom + numeroh
+porcentajesm = (numerom/total)*100
+porcentajesh = (numeroh/total)*100
+print("""
+Total de alumnos inscritos: %d
+Porcentaje de mujeres inscritas: %5.2f%%
+Porcentaje de hombres inscritos: %5.2f%%""" % (total, porcentajesm, porcentajesh))
