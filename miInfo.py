ef29de197c2cac742860dc22e2a62fa097b9ec6b
A01748151 <42352941+A01748151@users.noreply.github.com>
2018-08-20 22:15:53 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Alberto Contreras Torres, A01748151
+# Descripcion: Escribe un programa que muestre en la pantalla la siguiente información:
+•	Tu nombre completo.
+•	Tu matrícula.
+•	Tu carrera.
+•	Tu escuela de procedencia (Preparatoria, si es Prepa Tec el programa que cursaste)
+•	Dos o tres líneas que te describan de manera general (gustos, habilidades, deportes, libros preferidos, viajes, etc.)
 
 # Escribe tu programa después de esta línea.
+print("Nombre: Alberto Contreras Torres")
+print("Matrcula: A01748151")
+print("Carrera: ISD")
+print("Escuela de procedencia: UVM (Lago de Guadalupe)")
+print("""Descripcion: Me gustan la tecnologia, la música, animales.
+Me gusta practicar Taekwondo.""")
