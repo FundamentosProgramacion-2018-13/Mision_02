f4e8ab5128c0a7ef4d4df16c7915e2b3b6c37658
A01376466 <42380929+A01376466@users.noreply.github.com>
2018-08-20 19:40:46 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Ivan Honc Ayón, A01376466
+# Descripcion: Este programa calcula distancias y tiempos específicos a partir de una velocidad dada.
 
 # Escribe tu programa después de esta línea.
+
+velocidad = float(input("Escribe la velocidad que requieres para hacer los cálculos: "))
+
+distancia1 = velocidad*7
+distancia2 = velocidad*4.5
+tiempo1 = 791.00/velocidad
+
+print("""
+Tu velocidad es: %d
+Tu distancia recorrida después de 7 horas es de: %5.2f
+Tu distancia recorrida después de 4.5 horas es de: %5.2f
+El tiempo necesario para recorrer 791km a esta velocidad es de: %5.2f""" % (velocidad, distancia1, distancia2, tiempo1))
