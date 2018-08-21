ac1577790758207cc9c8f492a7bdead5d92d73ca
Mariana Paola Caballero Cabrera A01376544 <42322583+marianac99@users.noreply.github.com>
2018-08-20 21:52:41 -0500

    Create coordenada.py

---
+++ coordenada.py
@@ -0,0 +1,14 @@
+# Autor: Mariana Caballero, A01376544
+#Descripción: A continuación voy a crear un programa que me ayude a calcular la distancia entre 2 puntos 
+
+
+#Programa
+
+X1 = int (input("Coordenada en X del punto A:")
+Y2 = int (input("Coordenada en Y del punto A:")
+X2 = int (input("Coordenada en X del punto B:")
+Y2 = int (input("Coordenada en Y del punto B:")
+
+distancia = (((x2-x1)**2)+((y2-y1)**2)))**1/2
+
+print ("Distancia: %5.2f"  %(distancia)
