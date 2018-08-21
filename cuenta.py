84f3f6a1d594eb909cbcaaafd288d51d1298c8ee
geolib8 <42380925+geolib8@users.noreply.github.com>
2018-08-21 10:58:33 -0500

    Jesús Emmanuel Alcalá Nava
    A01376766
    grupo02

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jesús Emmanuel Alcalá Nava, A01376766
+# Descripcion: da al usuario el total de la cuenta con IVA y propina
 
 # Escribe tu programa después de esta línea.
+subtotal= float(input("costo de la comida:"))
+propina= subtotal*.13
+iva= subtotal*.15
+total= subtotal+propina+iva
+print("""
+propina: $%.02f
+IVA: $%.02f
+total: $%.02f""" %(propina, iva, total))
