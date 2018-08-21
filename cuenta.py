4270209affd18ba581be063b49413e853c8f2a3f
A01377230 <42380966+A01377230@users.noreply.github.com>
2018-08-20 21:07:23 -0500

    cuenta.py
    Jesús Roberto Herrera Vieyra
    A01377230
    Grupo 2

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jesús Roberto Herrera Vieyra, A01377230
+# Descripcion: Programa para calcular el subtotal, propina, IVA y total de una comida
 
 # Escribe tu programa después de esta línea.
+costo = float(input("Costo de la comida: "))
+
+propina= costo*0.13
+iva= costo*0.15
+costo_total= costo+propina+iva
+
+print("costo: $" "{0:.2f}".format(costo))
+print("Propina: $", "{0:.2f}".format(propina))
+print("IVA: $", "{0:.2f}".format(iva))
+print("Total: $", "{0:.2f}".format(costo_total))
