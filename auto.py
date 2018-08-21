e99257faa8ff5fae5bd510029fd1e913f5131615
Patricio-Mondragon <42380960+Patricio-Mondragon@users.noreply.github.com>
2018-08-21 18:36:52 -0500

    Programa 2 misión 2
    Patricio Mondragón A01748352 Grupo 02

--- auto.py
+++ auto.py
@@ -2,3 +2,12 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+Velocidad = int(input("Introduzca la velocidad del coche:"))
+Distanciarecorrida4.5hrs = Velocidadf*4.5
+Distanciarecorridaen7hrs= Velocidad*7
+Tiempoenhorasquerequierepara791km = 791/Velocidad
+
+print("Velocidad de el auto en km/h:", Velocidad)
+print("Distancia recorrida en 7 hrs:", Distanciarecorridaen7hrs)
+print("Distancia recorrida en 4.5 hrs:", Distanciarecorrida4.5hrs)
+print("Tiempo para recorrer 791km:", Tiempoenhorasquerequierepara791km )
