6fa62f8c06f0e4b8a4256fe9af6fe122c57d2ab2
Alex Leyva <42352939+A01747078@users.noreply.github.com>
2018-08-20 21:58:42 -0500

    MISION02-miinfo
    Alex Fernando Leyva Martinez, A01747078

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,22 @@
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: Alex Fernando Leyva Martinez, A01747078
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+Mostrar los datos requeridos en el problema
 # Escribe tu programa después de esta línea.
+print("""Nombre Completo: 
+Alex Fernando Leyva Martínez """)
+
+print("""Matrícula:
+A01747078""")
+
+print("""Carrera: 
+ISC""")
+
+print("""Escuela de Procedencia:
+Centro Escolar del Lago""")
+
+print("""Descripción: 
+Me gusta las actividades deportivas tales como la natación, el deporte y el atletismo, 
+además me interesan ciertos tópicos relacionados con la tecnología, matemáticas y el 
+pensamiento analítico, por otra parte disfruto mucho de mi familia y de todo lo nuevo 
+que puedo aprender.""" )
+
