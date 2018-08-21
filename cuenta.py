8ab547ec73bc9c4dc99b4c6f86e19bbfebe0f5df
jlmata <42380985+jlmata@users.noreply.github.com>
2018-08-21 17:19:39 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jose Luis Mata Lomeli, A01377205 
+# Descripcion: Sacar la propina y el IVA del precio de una comida
 
 # Escribe tu programa después de esta línea.
+
+Comida= int(input("Escriba el costo de la comida: "))
+
+Propina= Comida*0.13
+IVA= Comida*0.15
+
+Total_Pagar= Comida + Propina + IVA
+
+print("Costo de la Comida: ", (Comida))
+print("Propina: ", (Propina))
+print("IVA: ", (IVA))
+print("Total a pagar: ", (Total_Pagar)) 
