b55b931c6a8e6ab532a52434a2677674cbe550e1
Mariana Paola Caballero Cabrera A01376544 <42322583+marianac99@users.noreply.github.com>
2018-08-20 21:41:14 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Mariana Caballero, A01376544 
+# Descripcion: A continuación voy a crear un programa que calcule el número de personas que hay en un grupo, así como el porcentaje de mujeres y hombres dentro de ese grupo
 
 # Escribe tu programa después de esta línea.
+
+mujeres = input int("Cuántas mujeres hay?: ")
+hombres = input int("Cuántos hombres hay?: ")
+
+totalPersonas = mujeres+hombres
+porcentajeMujeres = mujeres*100/totalPersonas
+porcentajeHombres = hombres*100/totalPersonas
+
+print ("Total de inscritos:" , totalPersonas)
+print ("Porcentaje de mujeres inscritas: %5.2f"  %(porcentajeMujeres  "%")
+print ("Porcentaje de hombres inscritos: %5.2f"  %(porcentajeHombres  "%")
