39f316743f284e46ca777f964e2fd1453377196b
DianaBravo <42352948+DianaBravo@users.noreply.github.com>
2018-08-20 21:44:39 -0500

    Add files via upload
    Diana Marisol Medina Bravo}
    A01748753
    Grupo 04

---
+++ Distancia.py
@@ -0,0 +1,9 @@
+
+x1=int(input("Introduzca el valor de x1:"))
+x2=int(input("Introduzca el valor de x2:"))
+y1=int(input("Introduzca el valor de y1:"))
+y2=int(input("Introduzca el valor de y2:"))
+
+d=(((x2-x1)**2)+((y2-y1)**2))**0.5
+
+print("La distancia entre los dos puntos es: %5.4f" % d)
