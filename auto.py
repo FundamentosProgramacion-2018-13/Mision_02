f2afe153df9dafb2e49d96f6f44318ca69a5b774
A01377436 <42380908+A01377436@users.noreply.github.com>
2018-08-21 08:08:15 -0500

    Mision-02
    David Alejandro Nicolás Palos
    A01377436
    Grupo 02

--- auto.py
+++ auto.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que est√°s resolviendo.
+# Autor: David Alejandro Nicol·s Palos, A01377436
+# Descripcion: Escribir un programa que pregunte al usuario la velocidad a la que viaja un auto (km/h) y calcule e imprima la informaciÛn requerida
 
 # Escribe tu programa despu√©s de esta l√≠nea.
+
+print("Velocidad:")
+v=int(input())
+print("Velocidad en km/h:",v)
+Distancia1=v*7
+Distancia2=v*4.5
+Tiempo1=791/v
+print("Distancia recorrida en 7 hrs:",Distancia1)
+print("Distancia recorrida en 4.5 hrs:",Distancia2)
+print("Tiempo para recorrer 791 km:",Tiempo1)
--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: David Alejandro Nicol�s Palos, A01377436
+# Descripcion: Escribir un programa que calcule e imprima el costo total de una cuenta
 
 # Escribe tu programa después de esta línea.
+
+print("Costo de tu comida:")
+CostoC=int(input())
+Costo13=CostoC*.13
+Costo15=CostoC*.15
+Costo128=CostoC*1.28
+print("Costo de su comida:",CostoC)
+print("Propina:",Costo13)
+print("IVA:",Costo15)
+print("Total:",Costo128)
