90afebe88e43bb989180595efabf4080a55b1518
HCG <42352933+A01377246@users.noreply.github.com>
2018-08-20 21:52:32 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Humberto Carrillo Gómez, A01377246
+'''Descripcion: Este programa calcula la distancia que será recorrida en 7 y 4.5 hrs, además, calcula el tiempo para
+                recorrer 791 km'''
 
 # Escribe tu programa después de esta línea.
+
+velocidad=int(input("Teclea tu velocidad: "))
+distancia7= velocidad*7
+distancia4_5= velocidad*4.5
+tiempo791= 791/velocidad
+
+print("Tu velocidad es:", velocidad,"km/h")
+print("La distancia recorrida en 7 horas es:",format(distancia7,".1f"),"km/h")       # Resultados con un solo decimal
+print("La distancia recorrida en 4.5 horas es:",format(distancia4_5,".1f"),"km/h")
+print("El tiempo necesario para recorrer 791 km es:",format(tiempo791,".1f"), "hrs")
