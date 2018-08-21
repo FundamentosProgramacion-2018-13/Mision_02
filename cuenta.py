35b0e72266b95acf26853bd80155d8d36f4c7606
AlekHowland <42352824+AlekHowland@users.noreply.github.com>
2018-08-20 22:26:12 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,27 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Alek Fernando Howland Aguilar, A01747765
+# Descripcion: Unprograma que calcula el costo total de una comida en un restaurante.
 
 # Escribe tu programa después de esta línea.
+
+print ("Bienvenido")
+print("..............")
+
+subtotal = float(input("Cual fue el total de la cuenta:"))
+
+prop = 13*subtotal/100
+iva = 15*subtotal/100
+total = subtotal+prop+iva
+
+print("..............")
+
+print ("Subtotal de:", subtotal)
+print ("Fueron de IVA:%.2f" % (iva))
+print ("Fue de propina: %.2f" % (prop))
+print ("Total de: %.2f" % (total))
+print("..............")
+
+print ("Gracias")
+
+
+
+
