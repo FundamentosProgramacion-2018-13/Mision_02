a725b16244c5401b58ba3f7212cbcd9074654c1b
Carlos21Reyes <42328882+Carlos21Reyes@users.noreply.github.com>
2018-08-20 22:35:02 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,11 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Carlos Alberto Reyes Ortiz , A01376349
+# Descripcion: Imprime algunos datos de mi persona; nombre, matricula, carrera, escuela de procedencia y una descripción corta.
 
 # Escribe tu programa después de esta línea.
+print("Nombre:Carlos Alberto Reyes Ortiz")
+print("Matricula: A01376349")
+print("Carrera:ISDR")
+print("Escuela de procedencia: Prepa TEC CEM, Programa Bicultural")
+print("""Descripción: Mi pasatiempo preferido es el deporte. Práctico basketball, natación y voy al gimnasio. 
+             Me gusta mucho crear y buscar mejores maneras de hacer las cosas, por eso mismo estoy 
+             estudiando robótica"""  )
