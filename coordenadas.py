3369e20e4aa4d694d84e0e1974c038621f389367
Michelle Sánchez <42380961+A01376622@users.noreply.github.com>
2018-08-21 14:32:53 -0500

    Add files via upload

--- coordenadas.py
+++ coordenadas.py
@@ -1,8 +1,3 @@
-# Autor: Michelle Sánchez Guerrero, A01376622
-# Descripcion: Programa que calcula la distancia entre dos puntos.
-
-# Escribe tu programa después de esta línea.
-
 x1 = int(input("x1: "))
 y1 = int(input("y1: "))
 x2 = int(input("x2: "))
