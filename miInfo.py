dbaea3602171767b8b111ed8b3355d630d980b52
Patricio-Mondragon <42380960+Patricio-Mondragon@users.noreply.github.com>
2018-08-21 18:08:29 -0500

    Programa 1 de la misión
    Patricio Mondragón Fernandez A01748352

--- miInfo.py
+++ miInfo.py
@@ -2,3 +2,7 @@
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+print("Nombre: Patricio Mondragón")
+print("Carrera: Isdr")
+print("Escuela de procedencia: Thomas Jefferson")
+print("Descripcion: Me gustan mucho los videojuegos y la tecnologia")
