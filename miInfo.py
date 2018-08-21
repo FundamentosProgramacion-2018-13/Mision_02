d300dd747c037534e58b49c818ea87549caa034d
Mariana Paola Caballero Cabrera A01376544 <42322583+marianac99@users.noreply.github.com>
2018-08-20 20:37:04 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,9 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Mariana Caballero Cabrera, A01376544
+# Descripcion: A continuación voy a hacer un programa que imprima mi nombre, mi  matricula, la carrera que estudio, la prepa de la que vengo y un poco a cerca de lo que me gusta hacer
 
 # Escribe tu programa después de esta línea.
+
+print ("Nombre: Mariana Caballero Cabrera")
+print ("Matricula: A01376544")
+print ("Carrera: Prpa Tec Bicultural CEM")
+print ("Descripción: A mi me gusta mucho la tecnología, amo precticar deporte, en especial el futbol soccer y me gusta ver series de TV")
