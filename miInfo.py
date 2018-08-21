f7c30792519e78aba92aac81d9cec3293cecf2cd
a01748155 <42364366+a01748155@users.noreply.github.com>
2018-08-20 21:13:44 -0500

    miInfo terminado
    Erick David Ramírez Martínez
    A01748155
    Grupo: 02

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,17 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Erick David Ramírez Martínez, A01748155
+# Descripcion: Elaborar un programa que me permita mostrar mi nombre completo, mi matrícula, la carrera que decidí cursar, mi escuela de procedencia y una breve descripción de mi
+
 
 # Escribe tu programa después de esta línea.
+
+print("""Nombre: 
+Erick David Ramírez Martínez""")
+print("""Matrícula: 
+A01748155""")
+print("""Carrera: 
+Ingeniería en Sistemas Digitales y Robótica ISDR""")
+print("""Escuela de procedencia: 
+Plantel Fundación Azteca""")
+print("""¿Cómo soy?
+Soy una persona tranquila que le encanta jugar videojuegos y ver anime.
+También me gusta nadar, la tecnología, salir con mis amigos y comer""")
