2772bdfe1678bb6e6a67d2665b786c636f7fad7c
DiegoAyalaH <42352899+DiegoAyalaH@users.noreply.github.com>
2018-08-21 16:55:13 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: Diego Armando Ayala Hernández
+# Descripcion: Impresión de datos personales(nombre, matrícula, carrera, preparatoria y gustos)
 # Escribe tu programa después de esta línea.
+print(""" 
+Nombre:
+Diego Armnado Ayala Hernández
+Matrícula:
+A01376727
+Carrera:
+ISDR
+Escuela de procedencia:
+Prepa Tec Cem, programa Internacional
+Descripción:
+Me gusta la ciencias en especial cosas relacionadas con el espacio. Así mismo me gusta leer comics y jugar videojuegos. 
+Mi deporte favorito es el basquetball
+""")
