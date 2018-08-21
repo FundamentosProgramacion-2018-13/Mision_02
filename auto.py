153c88bc5ee9f4c61d52fd04b2a462788897d34e
Mariana Paola Caballero Cabrera A01376544 <42322583+marianac99@users.noreply.github.com>
2018-08-20 21:03:49 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Mariana Caballero Cabrera, A01376544
+# Descripcion: A continuación voy a hacer un programa que resuelva la siguiente fórmula (v=d/t) con un dato que el usuario me va a proporcionar y otros que el problema trae incluidos
 
 # Escribe tu programa después de esta línea.
+
+velocidadAuto = input float("Velocidad a la que va el carro: ")
+
+distanciaSieteHoras = velocidadAuto*7
+distanciaCuatroHoras = velocidadAuto*4.5
+tiempo = 791/velocidadAuto
+
+print ( "Distancia recorrida en 7 hrs: %5.2f" %(distanciaSieteHoras), "km")
+print ( "Distancia recorrida en 4.5 hrs: %5.2f" %(distanciaCuatroHoras), "km")
+print ( "Tiempo en recorrer 791 km: %5.2f" %(tiempo), "hrs")
