27a1b9dcc3bc4a53ba431d95135d7ceb082f5067
A01376354 <42328701+A01376354@users.noreply.github.com>
2018-08-20 22:31:06 -0500

    Update miInfo.py
    MiInfo Damián García, A01376354, Grupo04

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Damián Iván García Ravelo, A01376354
+# Descripcion: Mostrar datos acerca de mi.
 
 # Escribe tu programa después de esta línea.
+
+print ("""Nombre:                       
+Damián Iván García Ravelo""")
+print ("""Matrícula:
+A01376354""")
+print ("""Carrera:
+ISDR""")
+print ("""Escuela de procedencia:
+Prepa Tec CEM""")
+print ("""Descripción:
+Soy alguien al cual le gusta muchos los deportes, principalmente el basketball. Soy alguien apasionado por los videojuegos,
+mi libro favorito se llama 'El antiguo mundo'. Mi mejor aventura fue cuando recorrí Europa por 1 mes.""")
