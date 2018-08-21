1de1f808ec587f37ae50da45de8cf1c7370dd93c
geolib8 <42380925+geolib8@users.noreply.github.com>
2018-08-21 10:45:23 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -3,9 +3,9 @@
 
 # Escribe tu programa después de esta línea.
 velocidad1= float(input("introduzca la velocidad del auto en términos de km/hr"))
-distancia1= velocidad*7
-distancia2= velocidad*4.5
-tiempo1= 791/velocidad 
+distancia1= velocidad1*7
+distancia2= velocidad1*4.5
+tiempo1= 791/velocidad1 
 print("distancia recorrida en 7 hrs= %.1f km" % (distancia1))
 print("distancia recorrida en 4.5 hrs= %.1f km" % (distancia2))
 print("tiempo en el que se recorren 791 km= %.1f hrs" % (tiempo1))
