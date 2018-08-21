0056a6a853bbfef05da98dc76e8bf185b6fb3f8c
DiegoAyalaH <42352899+DiegoAyalaH@users.noreply.github.com>
2018-08-21 17:31:03 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: Diego Armando Ayala Hernández A01376727
+# Descripcion: Este Programa te ayuda a sacar el iva y propina de una comida, Primero pide el costo. Despues multiplica por .13 y .15 pra sacar propina e iva y por ultimo suma el costo, iva y propina para sacar el total
 # Escribe tu programa después de esta línea.
+costo=input("Dame el costo de la comida: ")
+subtotal=int(costo)
+
+iva=subtotal*.15
+propina=subtotal*.13
+total =subtotal+iva+propina
+print("Subtotal: $",subtotal)
+print("IVA: $ ", iva)
+print("Propina: $", propina)
+print("Total: $", total)
