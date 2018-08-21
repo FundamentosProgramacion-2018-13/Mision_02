4ab2f51dcc158211302b105010104d4d3e870477
geolib8 <42380925+geolib8@users.noreply.github.com>
2018-08-21 10:19:22 -0500

    Jesús Emmanuel Alcalá Nava
    A01376766
    grupo02

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jesús Emmanuel Alcalá Nava, A01376766
+# Descripcion: Imprimir información personal.
 
 # Escribe tu programa después de esta línea.
+print("nombre:")
+print("Jesús Emmanuel Alcalá Nava")
+print("matrícula:")
+print("A0137676")
+print("carrera:")
+print("ISC")
+print("escuela de procedencia:")
+print("preparatoria tec multicultural")
+print("descripción:")
+print("me gustan los deportes, en especial el football americano y el soccer, me gustan los videojuegos y viajar")
+
