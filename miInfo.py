ef6ec7ad6f9a0460f158f3ec18105df017abcb03
DanCordova <42380930+DanCordova@users.noreply.github.com>
2018-08-21 19:22:25 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: Daniel Córdova Bermúdez, A01377242
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
-# Escribe tu programa después de esta línea.
+print("Nombre:")
+print("Daniel Córdova Bermúdez.")
+print("Matrícula:")
+print("A01377242")
+print("Carrera:")
+print("ISDR")
+print("Escuela de procedencia::")
+print("Prepa Tec Multicultural")
+print("Descripción:")
+print("Me gusta la nueva tecnología junto con el diseño. Además, me encanta la arquitectura y las ciudades. Practico natación en mis tiempos libres.")
