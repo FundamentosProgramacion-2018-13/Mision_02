a743d0f30b13ed4c86cc1b193e6f0a6530fe478e
Maria Fernanda Vela Calderon <A01377958@itesm.mx>
2018-08-21 22:01:57 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,23 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: María Fernada Vela Calderón, A01377958
+# Descripcion: Realizar un programa que imprima mi información
 
 # Escribe tu programa después de esta línea.
+print ("""
+
+Nombre:
+María Fernada Vela Calderón
+
+Matrícula:
+A01377958
+
+Carrera:
+ISDR
+
+Escuela de procedencia:
+Prepa Tec Zona Esmeralda (Programa Bicultural)
+
+Descripción:
+Me gusta la robótica, aunque aún tengo mucho que aprender.
+Mi deporte favorito es la equitación.
+
+""")
