57b28813439ec3ff66d02af60f4a86830a808808
a01376331 <42352916+a01376331@users.noreply.github.com>
2018-08-21 21:36:59 -0500

    Update coordenadas.py

--- coordenadas.py
+++ coordenadas.py
@@ -1,3 +1,7 @@
+# Autor: Rubén Villalpando Bremont, A01376331, grupo 4
+# Descripcion: Programa que le pregunta al usuario las coordenadas de 2 puntos y el programa le devuelve la distancia entre ellos.
+
+# Escribe tu programa después de esta línea
 x1 = int(input("Escriba las coordenadas del punto X1: "))
 y1 = int(input("Escriba las coordenadas del punto Y1: "))
 x2 = int(input("Escriba las coordenadas del punto X2: "))
