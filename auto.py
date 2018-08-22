50dd5527390b37de720bf0060dde1cee8704acdf
DanhelMerVe <33494002+DanhelMerVe@users.noreply.github.com>
2018-08-21 23:27:02 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,22 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Danhel Alejandro Mercado Velasco
+Análisis.
+Entradas: Un auto va a 225 km
 
-# Escribe tu programa después de esta línea.
+Salidas: 
+La distancia en km. que recorre en 7 hrs.
+La distancia en km. que recorre en 4.5 hrs.
+El tiempo en horas que requiere para recorrer 791 km
+
+Relación E/S: v = d/t
+
+Inserta aquí la imagen con el algoritmo. (foto, captura de pantalla, texto, etc.)
+
+Un auto va a 225
+Distancia recorrida en 7 hrs 
+7 * 225= 1575.0
+Distancia recorrida en 4.5 hrs 
+4.5*225=1012.5
+Tiempo para recorrer 791
+Usar formula v = d/t
+Sustituir valores v=225, d=791 y t=?
+225=791/t  t=3.51hrs
