ea65ca64b2928d75179270a5597db82082e3385b
a01376331 <42352916+a01376331@users.noreply.github.com>
2018-08-21 21:12:49 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Rubén Villalpando Bremont, A01376331, grupo 4
+# Descripcion: El programa le pide al usuario que ingrese el número de hombres y mujeres que hay inscritos y le devuelve el número total
+#de alumnos inscritos y el porcentaje de hombres y mujeres.
 
 # Escribe tu programa después de esta línea.
+numeroMujeres = int(input("Ingrese el número de mujeres inscritas: "))
+numeroHombres = int(input("Ingrese el número de hombres inscritos: "))
+totalInscritos = numeroHombres + numeroMujeres
+porcentajeHombres = (numeroHombres/totalInscritos)*100
+porcentajeMujeres = (numeroMujeres/totalInscritos)*100
+print("Mujeres inscritas: ", numeroMujeres)
+print("Hombres inscritos: ", numeroHombres)
+print("Total de inscritos: ", totalInscritos)
+print("Porcentaje de mujeres: ", porcentajeMujeres, "%")
+print("porcentaje de hombres: ", porcentajeHombres, "%")
