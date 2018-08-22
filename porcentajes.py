77eb5c7ecc3a5a21a03936fceb9eba9df16fedc6
geolib8 <42380925+geolib8@users.noreply.github.com>
2018-08-21 20:50:48 -0500

    Jesús Emmanuel Alcalá Nava
    a01376766

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jesús Emmanuel Alcalá Nava , a01376766
+# Descripcion: calcular el total de alumnos y el porcentaje de hombres y mujeres de un grupo
 
 # Escribe tu programa después de esta línea.
+hombres= int(input("hombres insctritos:"))
+mujeres= int(input("mujeres inscritas:"))
+total= hombres+mujeres
+p1= float((hombres/total)*100)
+p2= float((mujeres/total)*100)
+print("total de alumnos:", total)
+print("porcentaje de hombres: %.1f" % p1, "%") 
+print("porcentaje de mujeres: %.1f" % p2, "%") 
