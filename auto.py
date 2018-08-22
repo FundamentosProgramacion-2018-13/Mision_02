86725c6f9530b813dbac8489c919ae40716710ec
geolib8 <42380925+geolib8@users.noreply.github.com>
2018-08-21 21:44:52 -0500

    Jesús emmanuel alcalá nava
    a01376766
    grupo02

--- auto.py
+++ auto.py
@@ -1,11 +1,13 @@
 # Autor: Jesús Emmanuel Alcalá Nava, a01376766
 # Descripcion: calcular la distancia y el tiempo que recorre un carro en una velocidad dada por el usuario
 
+
 # Escribe tu programa después de esta línea.
-velocidad1= float(input("introduzca la velocidad del auto en términos de km/hr")) 
-distancia1= velocidad1*7
-distancia2= velocidad1*4.5
-tiempo1= 791/velocidad1 
+velocidad1 = float(input("introduzca la velocidad del auto en términos de km/hr:"))
+distancia1 = velocidad1 * 7
+distancia2 = velocidad1 * 4.5
+tiempo1 = 791 / velocidad1
 print("distancia recorrida en 7 hrs= %.1f km" % (distancia1))
 print("distancia recorrida en 4.5 hrs= %.1f km" % (distancia2))
-print("tiempo en el que se recorren 791 km= %.1f hrs" % (tiempo1)) 
+print("tiempo en el que se recorren 791 km= %.1f hrs" % (tiempo1))
+
