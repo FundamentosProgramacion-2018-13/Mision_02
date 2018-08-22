84c150cdc8c71dce35212abc96ecd08d5e0c8850
A01377451 <42380944+A01377451@users.noreply.github.com>
2018-08-21 22:33:45 -0500

    cuenta.py
    Jocelyn López Ortíz 
    A01377451
    Grupo 02

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jocelyn López Ortíz, A01377451
+# Descripcion: Cálculo del costo de una comida considerando propina e IVA
 
 # Escribe tu programa después de esta línea.
+costo = int(input("Costo de su comida: "))
+
+subtotal = costo
+propina = costo*0.13
+IVA = costo*0.15
+total = subtotal+propina+IVA
+
+print("Propina: $" , propina)
+print("IVA: $", IVA)
+print ("Total a pagar: $", total)
