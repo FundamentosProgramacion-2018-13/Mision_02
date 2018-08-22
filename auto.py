3b0026d227d1d9fd4a7276926aaed4b086e11e4a
Maria Fernanda Vela Calderon <A01377958@itesm.mx>
2018-08-21 22:52:14 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: María Fenanda Vela Calderón, A01377958
+# Descripcion: Calcular distancia y tiempo que viaja el auto
 
 # Escribe tu programa después de esta línea.
+
+v = int( input( "Escribe la velocidad del auto: " ))
+
+r1 = v*7     #Recorrido 1
+
+r2 = v*4.5     #Recorrido 2
+
+t = 791/v
+
+print( "En 7hrs el auto recorrió: %5.01f km"  % r1 )
+print( "En 4.5hrs el auto recorrió:  %5.01f km" % r2)
+print( "Para recorrer 791km el auto tardó: %5.01f h" % t)
