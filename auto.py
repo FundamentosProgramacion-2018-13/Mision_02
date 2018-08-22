63cb4f0da6d5b25cbd8bdcb3a736aafc002f1cdb
ElAnbals <42352951+ElAnbals@users.noreply.github.com>
2018-08-21 21:49:08 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,8 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Rodolfo Anibal Altamirani Sánchez     A01377949
+# Descripcion: se te da la distancia y el tiempo, y se tiene que calcular la velocidad a la que se mueve el auto
 
 # Escribe tu programa después de esta línea.
+d = float(input("km recorridos: "))
+t = float(input("horas recorridas: "))
+v = d/t
+print("Tu velocidad es de %.2f:"%v,"km/h")
