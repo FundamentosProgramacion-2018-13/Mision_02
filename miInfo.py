1f5e120d052b2c654f1f99003c5fc4cc233d45a6
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2018-08-21 22:55:25 -0500

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,11 +1,16 @@
 # Autor: Juan Carlos Flores García, A01376511
-# Descripcion: Imprimir mis datos personales como nombre completo, matrícula, carrera, escuela de procedencia y gustos/habilidades.
+# Descripcion: Imprimir mis datos personales como nombre completo, matrícula, carrera, escuela de procedencia y descripción.
 
 # Escribe tu programa después de esta línea.
-print ("Juan Carlos Flores García")
-print ("A01376511")
-print ("Ingeniería en Sistemas Digitales y Robótica")
-print ("Prepa Tec, Programa Multicultural")
-print ("""Mis gustos son dibujar, jugar videojuegos y ver películas. Mis habilidades son hablar inglés bastante bien, tener conocimientos
+print ("""Nombre:
+       Juan Carlos Flores García""")
+print ("""Matrícula:
+       A01376511""")
+print ("""Carrera:
+        Ingeniería en Sistemas Digitales y Robótica""")
+print ("""Escuela de Procedencia:
+       Prepa Tec, Programa Multicultural""")
+print ("""Descripción:
+Mis gustos son dibujar, jugar videojuegos y ver películas. Mis habilidades son hablar inglés bastante bien, tener conocimientos
 previos de robótica y mucha creatividad. Mis deportes favoritos son natación y Taekwondo. Mi libro favorito es el Retrato de Dorian Grey y
 he viajado a Texas en Estados Unidos.""")
