6d68e00fc3088eb1bf25058af810a3d72fd09471
DanCordova <42380930+DanCordova@users.noreply.github.com>
2018-08-21 20:21:26 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Daniel Córdova Bermúdez, A01377242
+# Descripcion: El programa calcula la propina, iva y el total del consumo.
 
 # Escribe tu programa después de esta línea.
+
+subtotal = float(input("Total del consumo:"))
+
+propina = subtotal*0.13
+iva = subtotal*0.15
+total = subtotal+propina+iva
+                 
+print("Costo de su comida:",subtotal)
+print("Propina:",propina)
+print("IVA:",iva)
+print("Total a pagar:",total)
