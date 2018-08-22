de1eb7c88934de8319f1e38a22f726ebef4e9cb3
A01377718 <42326546+A01377718@users.noreply.github.com>
2018-08-21 21:06:18 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,17 @@
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: Javier Alexandro Vargas Sánchez, A01377718
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
 
 # Escribe tu programa después de esta línea.
+costoComida= (int(input("Teclea el subtotal de tu comida:  ")))
+
+
+propina = costoComida*.13
+
+iva = costoComida *.15
+
+costoTotal = propina + iva +costoComida
+
+print("El subtotal fue: ", costoComida)
+print("El Iva fue de:  ", iva)
+print ("Dejaste esta cantidad de propina:  ", propina)
+print ("El costo total de tu comida fue de: ", costoTotal)
