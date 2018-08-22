5e9b457e75a7dfdcd0f00bcfc07a8e626e8da839
a01376331 <42352916+a01376331@users.noreply.github.com>
2018-08-21 20:22:02 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Rubén Villalpando Bremont, A01376331, grupo 4
+# Descripcion: Programa que le pregunta al usuario la velocidad a la que viaja un auto y le devuelve unos datos específicos.
 
-# Escribe tu programa después de esta línea.
+# Escribe tu programa después de esta línea
+velocidad = int(input("Escriba la velocidad a la que va un vehículo en km/h: "))
+distancia_7horas = velocidad*7
+distanciaCuatroHorasYMedia = velocidad*4.5
+tiempoQueTarda = 791/velocidad
+
+print("La distancia recorrida en 7 horas es: ", distancia_7horas, "km")
+print("La distancia recorrida en 4.5 horas es: ", distanciaCuatroHorasYMedia, "km")
+print("El tiempo que se tarda en recorrer 791km. es de ", tiempoQueTarda, "horas")
+
