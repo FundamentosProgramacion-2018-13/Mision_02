6724be9958a2425fdb21635a8ffebd8614242010
ElAnbals <42352951+ElAnbals@users.noreply.github.com>
2018-08-21 21:44:55 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,13 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Rodolfo Anibal Altamirano Sánchez     A01377949
+# Descripcion: se te proporcionan los datos de cantdad de hombres y mujeres que hay en una clases y se necesita calcular el total y el porcentaje de cada genero
 
 # Escribe tu programa después de esta línea.
+hombres = int(input("Cantidad de Hombres:"))
+mujeres = int(input("Cantidad de mujeres:"))
+total = hombres + mujeres
+porcentajeH = (hombres*100)/total
+porcentajeM = 100-porcentajeH
+
+print("Alumnos inscritos:",total)
+print("Porcentaje de hombres %.2f"%porcentajeH,"%")
+print("Porcentaje de mujeres %.2f"%porcentajeM,"%")
