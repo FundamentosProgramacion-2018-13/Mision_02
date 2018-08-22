0c27cb4cd75f8a0054715dbf995823c047c38dc7
AlexysCoate <42352943+AlexysCoate@users.noreply.github.com>
2018-08-21 22:41:03 -0500

    Add files via upload

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,26 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor:Alexys Martín Coate Reyes, A01746998
+
+# Descripcion: Calcular el total a pagar de una cuenta dee restaurante, contando el iva y la propina.
+
+"""Elabora un algoritmo y escribe un programa que calcula el costo total de una comida en un restaurante.
+# •	El programa le pregunta al usuario el total de la comida.
+# •	Agrega 13% de propina y 15% de IVA.
+# •	Cada porcentaje se calcula con respecto al costo de la comida.
+# •	Imprime:
+# o	El subtotal (costo de la comida)
+# o	La propina.
+# o	El IVA.
+# o	El total a pagar. (Suma del subtotal, la propina y el IVA)"""
 
 # Escribe tu programa después de esta línea.
+
+subtotal = float(input("Ingresa el costo total de la comida: "))
+
+propina = subtotal*.13
+iva = subtotal*.15
+total = subtotal + iva + propina
+
+print("Subtotal: $%.2f" % (subtotal))
+print("Propina: $%.2f" % (propina))
+print("IVA: $%.2f" % (iva))
+print("Total a pagar: $%.2f" % (total))
--- miInfo.py
+++ miInfo.py
@@ -1,4 +1,23 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor:Alexys Martín Coate Reyes, A01746998
+#
+# Descripcion: Elaborar un programa que pueda imprimir la siguiente información
+
+"""Elabora un algoritmo y escribe un programa que muestre en la pantalla la siguiente información:
+# •	Tu nombre completo.
+# •	Tu matrícula.
+# •	Tu carrera.
+# •	Tu escuela de procedencia (Preparatoria, si es Prepa Tec el programa que cursaste)
+# •	Dos o tres líneas que te describan de manera general (gustos, habilidades, deportes, libros preferidos, viajes, etc.)"""
 
 # Escribe tu programa después de esta línea.
+
+print("""Nombre:
+Alexys Martín Coate Reyes
+Matrícula:
+A01746998
+Carrera:
+ISDR
+Escuela:
+Colegio Baden Powell
+Descripción:
+Me gusta tocar el violín y el piano, además de practicar deporte como natación o futbol. Igualmente me gusta jugar videojuegos y leer.""")
