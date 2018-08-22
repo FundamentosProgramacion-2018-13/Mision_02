7d3ff3f51ee03c59408bab057cba52ad2186c1d2
A01748151 <42352941+A01748151@users.noreply.github.com>
2018-08-21 20:10:51 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,16 @@
-# Autor: tuNombreCompleto, tuMatricula
+# Autor: Alberto Contreras Torres, A01748151
 # Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+•	La distancia en km. que recorre un cohe en 7 hrs.
+•	La distancia en km. que recorre un coche en 4.5 hrs.
+•	El tiempo en horas que requiere un cohce para recorrer 791 km.
+
 
 # Escribe tu programa después de esta línea.
+v= int(input("teclea velocidad :"))
+d7= v*7
+d4= v*4.5
+t791km= 791/v
+print("Velocidad del auto en km/h", v,"km/h")
+print("Distancia recorrida en 7hr = %0.1f"% d7,"km")
+print("Distancia recorrida en 4.5hr = %0.1f"% d4,"km")
+print("Tiempo para recorre 791km = %0.1f"% t791km,"hr")
