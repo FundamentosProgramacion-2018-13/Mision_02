7edf4355df6c33a1e20b19753b77e9af5023ca08
DanCordova <42380930+DanCordova@users.noreply.github.com>
2018-08-21 19:47:25 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,12 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Daniel Córdova Bermúdez, A01377242
+# Descripcion: El programa calcula la distancia en que recorre un auto a 7 horas y 4.5 además del tiempo para recorrer 791.
 
 # Escribe tu programa después de esta línea.
+
+velocidad = float(input("Velocidad en k/m:"))
+distancia_1 = velocidad * 7
+distancia_2 = velocidad * 4.5
+tiempo = 791 / 1
+print("Distancia recorrida en 7 horas:",distancia_1)
+print("Distancia recorrida en 4.5 horas;",distancia_2)
+print("Tiempo en recorrer en 791 km/h:",tiempo)
