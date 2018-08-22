9137b90821df041da47ffc7fac907ddd424177a6
a01376331 <42352916+a01376331@users.noreply.github.com>
2018-08-21 21:35:08 -0500

    Add files via upload

---
+++ coordenadas.py
@@ -0,0 +1,7 @@
+x1 = int(input("Escriba las coordenadas del punto X1: "))
+y1 = int(input("Escriba las coordenadas del punto Y1: "))
+x2 = int(input("Escriba las coordenadas del punto X2: "))
+y2 = int(input("Escriba las coordenadas del punto Y2: "))
+distanciaEntrePuntos = (((x2-x1)**2)+((y2-y1)**2))**0.5
+print("Distancia entre los puntos: ", distanciaEntrePuntos)
+
