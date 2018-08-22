435d88c9db0e79d268fe449a2c1d7af49277f0cf
DanCordova <42380930+DanCordova@users.noreply.github.com>
2018-08-21 22:20:02 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -7,6 +7,6 @@ velocidad = float(input("Velocidad en k/m:"))
 distancia_1 = velocidad * 7
 distancia_2 = velocidad * 4.5
 tiempo = 791 / velocidad
-print("Distancia recorrida en 7 horas:",distancia_1)
-print("Distancia recorrida en 4.5 horas;",distancia_2)
-print("Tiempo en recorrer en 791 km/h:",tiempo)
+print("Distancia recorrida en 7 horas:%.1f" %(distancia_1))
+print("Distancia recorrida en 4.5 horas:%.1f" %(distancia_2))
+print("Tiempo en recorrer en 791 km/h:%.1f" %(tiempo))
