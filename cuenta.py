ad7d3427ef7ff0191ca45d02305cb286377f9586
DanhelMerVe <33494002+DanhelMerVe@users.noreply.github.com>
2018-08-21 23:29:00 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,27 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Danhel Alejandro Mercado Velasco
+. Elabora un algoritmo y escribe un programa que calcula el costo total de una comida en un restaurante.
+•	El programa le pregunta al usuario el total de la comida.
+•	Agrega 13% de propina y 15% de IVA.
+•	Cada porcentaje se calcula con respecto al costo de la comida.
+•	Imprime:
+o	El subtotal (costo de la comida)
+o	La propina.
+o	El IVA.
+o	El total a pagar. (Suma del subtotal, la propina y el IVA)
 
-# Escribe tu programa después de esta línea.
+
+Análisis.
+Entradas: totalcomida $340
+Salidas: Total
+Relación E/S: IVA + Propina + totalcomida
+
+Inserta aquí la imagen con el algoritmo. (foto, captura de pantalla, texto, etc.)
+Totalcomida = $340.00
+calcular propina es (340 * 13 / 100)
+propina = 44.20
+calcular IVA es( 340 *15 / 100) 
+iva= 51.00
+Total es ( IVA + propina + totalcomida) 
+51.00 + 44.20 = 95.20 + 340 = $435.20
+Total = $435.20
+
