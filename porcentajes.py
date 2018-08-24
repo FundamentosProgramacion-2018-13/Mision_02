52a09472843cedc3037e656f02c1e31ec05d0afb
A01746763 <42380899+A01746763@users.noreply.github.com>
2018-08-24 15:36:56 -0500

    Update porcentajes.py

--- porcentajes.py
+++ porcentajes.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jonathan Sanabria Rocha, A01746763
+# Descripcion: Porcentajes de hombre y mujeres inscritos presentando los porcentajes con un solo decimal.
 
 # Escribe tu programa después de esta línea.
+hombres=float(input("Dame el numero de Hombre inscritos: "))
+mujeres=float(input("Dame el numero de mujeres inscritas: "))
+totinscritos=hombres+mujeres
+porch=hombres/totinscritos*100
+porcm=mujeres/totinscritos*100
+print("Hombres inscritos: %s" %(hombres))
+print("Mujeres inscritas: %s" %(mujeres))
+print("Total de inscritos: %s" %(totinscritos))
+print("Porcentaje de hombres: ",format(porch,".1f"))
+print("Porcentaje de mujeres: ",format(porcm,".1f"))
