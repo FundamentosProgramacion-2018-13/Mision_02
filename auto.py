eda62246486d46ddb7ced02c1ef439f2c0bd3335
A01746763 <42380899+A01746763@users.noreply.github.com>
2018-08-24 03:36:33 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,4 +1,15 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jonathan Sanabria Rocha, A01746763
+# Descripcion: Problema de auto con formula v=d/t , uso de format.
 
 # Escribe tu programa después de esta línea.
+velocidad= float(input("Dame la velocidad del auto en km/h:"))
+tiempo= float(input("Dame el tiempo de recorrido del auto en horas:"))
+distancia=(float(velocidad)/float(tiempo))
+tiempo1= input("Dame el tiempo de recorrido del auto en horas: ")
+distancia1=(float(velocidad)/float(tiempo1))
+distancia2=input("Dame la distancia que recorrera el auto: ")
+tiempo2=(float(distancia2)/float(velocidad))
+print("Velocidad del auto en km/h= ",format(velocidad,".1f")+" km/h")
+print("Distancia recorrida en "+str(float(tiempo))+" hrs:=",format(distancia,".1f")+" km")
+print("Distancia recorrida en "+str(float(tiempo1))+" hrs:=",format(distancia1,".1f")+" km")
+print("Tiempo para recorrer "+str(float(distancia2))+" km:=",format(tiempo2,".1f")+" hrs")
