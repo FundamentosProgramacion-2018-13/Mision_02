c45866a39d50a72f3d1494a367671d40ead9b339
A01746763 <42380899+A01746763@users.noreply.github.com>
2018-08-24 15:05:22 -0500

    Update cuenta.py

--- cuenta.py
+++ cuenta.py
@@ -1,4 +1,14 @@
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Jonathan Sanabria Rocha, A01746763
+# Descripcion: Cuenta de un restaurante, propina e IVA.
 
 # Escribe tu programa después de esta línea.
+subtotal= float(input("Dame el subtotal de la cuenta de la comida: "))
+porprop=.13
+iva=.15
+prop=subtotal*porprop
+totiva=subtotal*iva
+total=subtotal+prop+totiva
+print("Costo de su comida: %s" %(subtotal))
+print("Propina: "+str(float(prop)))
+print("Iva: "+str(float(totiva)))
+print("TOTAL: "+str(float(total)))
